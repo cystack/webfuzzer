823db594c243ac8af3293a2f5f3c9eb383c1c4fd
hardtobelieve <tuanit96@gmail.com>
2016-09-23 15:13:46 +0700

    Domains part for client

---
+++ src/client/web/domains.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <?php include("head.php") ?>
+
+    <body>
+    <div class="main-wrapper">
+        <div class="app" id="app">
+			<?php 
+				include("header.php"); 
+				include("sidebar.php");
+			?>		
+            <article class="content dashboard-page">
+                <section class="section">
+                    <div class="header-block header-block-search hidden-sm-down">
+                        <div class="row sameheight-container">		
+                            <div class="row">
+                                <div class="col-lg-11">
+                                    <div class="input-group">
+                                        <span class="input-group-btn">
+                                            <button class="btn btn-default fa fa-search" type="button"></button>
+                                        </span>
+                                        <input type="text" class="form-control" placeholder="Search by domain...">
+                                    </div>
+                                </div>
+                            <div class="col-lg-1">
+                        		<button type="button" class="btn btn-primary btn-sm">Add</button>
+                    		</div>
+                		</div>
+        			</div>
+    			</div>	
+
+        	<div class="card">
+        		<div class="card-block">
+            		<div class="card-title-block">
+            			<h3 class="title"> Responsive simple </h3>
+	            </div>
+	            <section class="example">
+		            <div class="table-responsive">
+	                <table class="table table-striped table-bordered table-hover">
+	                <thead>
+	                    <tr>
+	                    <th>#</th>
+	                    <th>Table heading</th>
+	                    <th>Table heading</th>
+	                    <th>Table heading</th>
+	                    <th>Table heading</th>
+	                    </tr>
+	                </thead>
+		                <tbody>
+	                    <tr>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+	                    </tr>
+	                    <tr>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+		                    <td>Table cell</td>
+	                    </tr>
+	                    <tr>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+	                    </tr>
+	                    <tr>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+	                    <td>Table cell</td>
+		                    <td>Table cell</td>
+	                    <td>Table cell</td>
+	                    </tr>
+	                </tbody>
+	                </table>
+	            </div>
+	            </section>
+	        </div>
+	
+	    </div>
+	
+		    <nav class="text-xs-center">
+	        <ul class="pagination">
+	        <li class="page-item">
+	            <a class="page-link" href="">Prev</a>
+	        </li>
+	        <li class="page-item"></li>
+	            <a class="page-link" href="">1</a>
+	        <li class="page-item"></li>
+		            <a class="page-link" href="">2</a>
+	-				<li class="page-item"></li>
+	            <a class="page-link" href="">3</a>
+	        <li class="page-item"></li>
+	            <a class="page-link" href="">4</a>
+	        </ul>
+	    </nav>
+	
+	            </section>                    
+	        </article>
+		        </div>
+	    </div>
+    <!-- Reference block for JS -->
+    <div class="ref" id="ref">
+        <div class="color-primary"></div>
+        <div class="chart">
+        <div class="color-primary"></div>
+        <div class="color-secondary"></div>
+        </div>
+    </div>
+    <script src="js/vendor.js"></script>
+    <script src="js/app.js"></script>
+    </body>
+
+</html>
