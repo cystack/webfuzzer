bd29f949df56f4b4e70f3fa9dd7bbf91c68344b4
hardtobelieve <tuanit96@gmail.com>
2016-09-24 23:52:44 +0700

    split functions

--- src/client/web/js/script.js
+++ src/client/web/js/script.js
@@ -1,35 +1,96 @@
-$(document).ready(function () {
-    //Initialize tooltips
-    $('.nav-tabs > li a[title]').tooltip();
-    
-    //Wizard
-    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+function resetActive(event, id, mode) {
+    if ( mode == "unfinished" ) {
+        document.getElementById(id).style.borderColor = "#C0C0C0";
+    }
+    else if ( mode == "finised" ) {
+        if ( id == "2" && document.getElementById("domain_field").value.length == 0 ) {
+            alert('Fill in the blank');
+            return;
+        }
+        else {
+            if ( id == "4" ) {
+                document.getElementById('target_domain').innerHTML = document.getElementById('domain_field').value;
+            }
+            document.getElementById(id-1).style.borderColor = "green";
+        }
+    }
 
-        var $target = $(e.target);
-    
-        if ($target.parent().hasClass('disabled')) {
-            return false;
+    $("div").each(function () {
+        if ($(this).hasClass("activestep")) {
+            $(this).removeClass("activestep");
         }
     });
 
-    $(".next-step").click(function (e) {
+    if (event.target.className == "col-md-3") {
+        $(event.target).addClass("activestep");
+    }
+    else {
+        $(event.target.parentNode).addClass("activestep");
+    }
 
-        var $active = $('.wizard .nav-tabs li.active');
-        $active.next().removeClass('disabled');
-        nextTab($active);
-
-    });
-    $(".prev-step").click(function (e) {
-
-        var $active = $('.wizard .nav-tabs li.active');
-        prevTab($active);
-
-    });
-});
-
-function nextTab(elem) {
-    $(elem).next().find('a[data-toggle="tab"]').click();
+    hideSteps();
+    showCurrentStepInfo('step-' +id);
 }
-function prevTab(elem) {
-    $(elem).prev().find('a[data-toggle="tab"]').click();
+
+function hideSteps() {
+    $("div").each(function () {
+        if ($(this).hasClass("activeStepInfo")) {
+            $(this).removeClass("activeStepInfo");
+            $(this).addClass("hiddenStepInfo");
+        }
+    });
+}
+function showCurrentStepInfo(step) {        
+    var id = "#" + step;
+    $(id).addClass("activeStepInfo");
+}
+function submitData() {
+    var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eSI6ImMwOWFjNzExLTgyYTYtNDE1Zi1iMGI5LTg2NTA3YTM2NDAxMyIsImlhdCI6MTQ3NDcxODk1OSwibmJmIjoxNDc0NzE4OTU5LCJleHAiOjE0NzQ4MDUzNTl9.VB7wbwn2q6Kntsz18xA_a7juCrEA6u-JS6uBUORmSac';
+    var domain = document.getElementById('domain_field').value;
+    var protocol;
+    var port;
+    var tmp = $("input[name=protocol]:checked").val();
+    if ( tmp == "https" ) protocol = "1";
+    else protocol = "0";
+    tmp = $("input[name=port]:checked").val();
+    if ( tmp == "Other" ) {
+        if ( document.getElementById('portText').value.length == 0 ) {
+            alert("Need a specific port");
+            return;
+        }
+    }
+}
+
+function submitData() {
+    var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eSI6ImMwOWFjNzExLTgyYTYtNDE1Zi1iMGI5LTg2NTA3YTM2NDAxMyIsImlhdCI6MTQ3NDcxODk1OSwibmJmIjoxNDc0NzE4OTU5LCJleHAiOjE0NzQ4MDUzNTl9.VB7wbwn2q6Kntsz18xA_a7juCrEA6u-JS6uBUORmSac';
+    var domain = document.getElementById('domain_field').value;
+    var protocol;
+    var port;
+    var tmp = $("input[name=protocol]:checked").val();
+    if ( tmp == "https" ) protocol = "1";
+    else protocol = "0";
+    tmp = $("input[name=port]:checked").val();
+    if ( tmp == "Other" ) {
+        if ( document.getElementById('portText').value.length == 0 ) {
+            alert("Need a specific port");
+            return;
+        }
+        port = document.getElementById('portText').value;
+    }
+    else port = tmp;
+    var http = new XMLHttpRequest();
+    var url = "http://188.166.224.165:5555/domains";
+    var e = document.getElementById('sb');
+    var params = '{ "url" : "' + domain + '", "description" : "' + e.options[e.selectedIndex].value + '", "port" : "' + port + '", "ssl" : "' + protocol + '" }';
+    http.open("POST", url, false);
+
+    http.setRequestHeader("Content-Type", "application/json");
+    http.setRequestHeader("Authorization", "JWT " + token);
+    http.send(params);
+    window.location.replace("domains.php");
+}
+
+function enableNext() {
+    document.getElementById("success-domain").style.display = 'block';
+    document.getElementById("firstNext").disabled = false;
 }
