b25e1c34544b1334577e98214222b64129e7c9a1
hardtobelieve <tuanit96@gmail.com>
2016-09-24 11:20:59 +0700

    Fix sidebar

--- src/client/web/sidebar.php
+++ src/client/web/sidebar.php
@@ -4,15 +4,16 @@
             <div class="brand">
                 <div class="logo"> <span class="l l1"></span> <span class="l l2"></span> <span class="l l3"></span> <span class="l l4"></span> <span class="l l5"></span> </div> WebFuzzer</div>
             </div>
+
             <nav class="menu">
                 <ul class="nav metismenu" id="sidebar-menu">
-                    <li class="active">
+                    <li <?php if ( basename($_SERVER['PHP_SELF']) == "dashboard.php" ) { echo "class=\"active\"";} ?> >
                         <a href="dashboard.php"> <i class="fa fa-home"></i> Dashboard </a>
                     </li>
-                    <li>
+                    <li <?php if ( basename($_SERVER['PHP_SELF']) == "domains.php" ) { echo "class=\"active\"";} ?> >
                         <a href="domains.php"> <i class="fa fa-th-large"></i> Domains </a>
                     </li>
-                    <li>
+                    <li <?php if ( basename($_SERVER['PHP_SELF']) == "scan.php" ) { echo "class=\"active\"";} ?> >
                         <a href="scan.php"> <i class="fa fa-search"></i> Scans </a>
                     </li>
                     <!-- <li>
@@ -26,16 +27,16 @@
                             </a> </li>
                         </ul>
                     </li> -->
-                    <li>
+                    <li <?php if ( basename($_SERVER['PHP_SELF']) == "vulnerability.php" ) { echo "class=\"active\"";} ?> >
                         <a href="./vulnerability.php"> <i class="fa fa-exclamation-triangle"></i> Vulnerabilities </a>
                     </li>
-                    <li>
+                    <li <?php if ( basename($_SERVER['PHP_SELF']) == "apikey.php" ) { echo "class=\"active\"";} ?> >
                     <a href="./apikey.php"> <i class="fa fa-key"></i> API Keys </a>
                     </li>
-                    <li>
+                    <li <?php if ( basename($_SERVER['PHP_SELF']) == "userProfile.php" ) { echo "class=\"active\"";} ?> >
                         <a href="userProfile.php"> <i class="fa fa-user"></i> User profile </a>
                     </li>
-                    <li>
+                    <li <?php if ( basename($_SERVER['PHP_SELF']) == "support.php" ) { echo "class=\"active\"";} ?> >
                         <a href="./support.php"> <i class="fa fa-life-ring"></i> Support </a>
                     </li>
                 </ul>
